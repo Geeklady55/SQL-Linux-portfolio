42ef73b0f372d4519a67f42d9cfb3af0e136717b
Geeklady55 <aiguru2002@gmail.com>
2026-03-25 20:38:44 -0600

    Update indexing_examples.sql

--- mysql/indexing_examples.sql
+++ mysql/indexing_examples.sql
@@ -1,1 +1,9 @@
+EXPLAIN
+SELECT order_id, customer_id, status
+FROM orders
+WHERE customer_id = 101
+  AND status = 'OPEN';
 
+
+CREATE INDEX idx_orders_customer_status
+ON orders(customer_id, status);
