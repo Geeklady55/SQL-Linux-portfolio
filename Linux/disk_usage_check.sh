c65caa4be2636fa8f5382542f62302c708ded98d
Geeklady55 <aiguru2002@gmail.com>
2026-03-25 20:40:02 -0600

    Update disk_usage_check.sh

--- Linux/disk_usage_check.sh
+++ Linux/disk_usage_check.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+
+THRESHOLD=80
+
+df -h | awk 'NR>1 {print $5 " " $1}' | while read output;
+do
+    usage=$(echo $output | awk '{print $1}' | sed 's/%//')
+    filesystem=$(echo $output | awk '{print $2}')
+
+    if [ "$usage" -ge "$THRESHOLD" ]; then
+        echo "WARNING: $filesystem is ${usage}% full"
+    fi
+done
