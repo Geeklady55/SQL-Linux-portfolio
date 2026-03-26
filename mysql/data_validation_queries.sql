a05eb279f6cc69e47ddaefb0c663c2ae3de93adf
Geeklady55 <aiguru2002@gmail.com>
2026-03-25 20:36:49 -0600

    Update data_validation_queries.sql

--- mysql/data_validation_queries.sql
+++ mysql/data_validation_queries.sql
@@ -1,1 +1,10 @@
 
+SELECT 
+    o.order_id,
+    o.total_amount,
+    SUM(oi.quantity * oi.unit_price) AS calculated_total
+FROM orders o
+JOIN order_items oi
+    ON o.order_id = oi.order_id
+GROUP BY o.order_id, o.total_amount
+HAVING o.total_amount <> SUM(oi.quantity * oi.unit_price);
