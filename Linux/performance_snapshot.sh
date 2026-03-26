8e3015cf1efb012f811134ed94b8977884be918d
Geeklady55 <aiguru2002@gmail.com>
2026-03-25 20:40:41 -0600

    Update performance_snapshot.sh

--- Linux/performance_snapshot.sh
+++ Linux/performance_snapshot.sh
@@ -1,1 +1,17 @@
 
+#!/bin/bash
+
+echo "===== LOAD ====="
+uptime
+
+echo ""
+echo "===== TOP CPU ====="
+ps aux --sort=-%cpu | head -10
+
+echo ""
+echo "===== TOP MEMORY ====="
+ps aux --sort=-%mem | head -10
+
+echo ""
+echo "===== NETWORK LISTENERS ====="
+ss -tulnp
