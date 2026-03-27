5ad2b12bb28e07c46884ecd7ec783abde7b37d53
Geeklady55 <aiguru2002@gmail.com>
2026-03-26 20:24:17 -0600

    Update mysql_health_check.sh

--- mysql/mysql_health_check.sh
+++ mysql/mysql_health_check.sh
@@ -1,1 +1,23 @@
 
+#!/bin/bash
+
+mysql -e "SHOW DATABASES;"
+
+mysql -e "SHOW STATUS LIKE 'Uptime';"
+
+mysql -e "SHOW STATUS LIKE 'Threads_connected';"
+
+mysql -e "SHOW STATUS LIKE 'Slow_queries';"
+
+mysql -e "SHOW VARIABLES LIKE 'max_connections';"
+
+
+Bonus Vwersion
+Add alert logic
+
+THREADS=$(mysql -N -e "SHOW STATUS LIKE 'Threads_connected';" | awk '{print $2}')
+
+if [ "$THREADS" -gt 100 ]
+then
+  echo "WARNING HIGH CONNECTION COUNT"
+fi
